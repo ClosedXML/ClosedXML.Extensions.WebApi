abde15f4ce3a87819e620be777262bc0f9f4e164
Francois Botha <igitur@gmail.com>
2018-05-14 14:28:41 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# ClosedXML.Extensions.WebApi
+WebApi Extensions for [ClosedXML](https://github.com/ClosedXML/ClosedXML)
+
+## Install via NuGet
+
+To install ClosedXML.Extensions.Mvc, run the following command in the Package Manager Console
+
+```
+PM> Install-Package ClosedXML.Extensions.WebApi
+```
+
+
+## Usage
+In your WebApi controller define an action that will generate and download your file:
+
+```c#
+public class ExcelController : ApiController
+{
+    [HttpGet]
+    [Route("api/file/{id}")]
+    public async Task<HttpResponseMessage> DownloadFile(int id)
+    {
+        var wb = await BuildExcelFile(id);
+        return wb.Deliver("excelfile.xlsx");
+    }
+
+    private async Task<XLWorkbook> BuildExcelFile(int id)
+    {
+        //Creating the workbook
+        var t = Task.Run(() =>
+        {
+            var wb = new XLWorkbook();
+            var ws = wb.AddWorksheet("Sheet1");
+            ws.FirstCell().SetValue(id);
+
+            return wb;
+        });
+
+        return await t;
+    }
+}
+```
---
+++ src/ClosedXML.Extensions.Mvc/WebApiExtensions.cs
@@ -0,0 +1,30 @@
+﻿using ClosedXML.Excel;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace ClosedXML.Extensions
+{
+    public static class WebApiExtensions
+    {
+        public static HttpResponseMessage Deliver(this XLWorkbook workbook, string fileName, string contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+        {
+            var memoryStream = new MemoryStream();
+            workbook.SaveAs(memoryStream);
+            memoryStream.Seek(0, SeekOrigin.Begin);
+
+            var message = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StreamContent(memoryStream)
+            };
+            message.Content.Headers.ContentType = new MediaTypeHeaderValue(contentType);
+            message.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
+            {
+                FileName = fileName
+            };
+
+            return message;
+        }
+    }
+}
